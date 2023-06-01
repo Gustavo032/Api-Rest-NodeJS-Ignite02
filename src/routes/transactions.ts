8ce9ae597697c5cc114a08bedfbe59f6b64eee4d
Gustavo032 <gustavoramosrock@gmail.com>
2023-06-01 18:00:43 -0300

    adicionando detalhes de transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -13,6 +13,25 @@ import { knex } from '../database'
 // }
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select('*')
+
+    // bom retornar sempre como objeto
+    return { total: transactions.length, transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return { transaction }
+  })
+
   app.post('/', async (request, reply) => {
     // validação dos dados vindo da req
     const createTransactionBodySchema = z.object({
