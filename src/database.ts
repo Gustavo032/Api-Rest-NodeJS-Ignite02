c9d5092efdd2120e06360b07c4999510f3415f98
Gustavo032 <gustavoramosrock@gmail.com>
2023-06-21 14:18:46 -0300

    prepare

--- src/database.ts
+++ src/database.ts
@@ -4,13 +4,16 @@ import { env } from './env'
 console.log(process.env.DATABASE_URL)
 
 export const config: Knex.Config = {
-  client: 'sqlite', // *obrigated*
+  client: env.DATABASE_CLIENT, // *obrigated*
   // *obrigated* ,
-  connection: {
-    filename: env.DATABASE_URL,
-  },
-  useNullAsDefault: true,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
 
+  useNullAsDefault: true,
   migrations: {
     extension: 'ts',
     directory: './db/migrations',
